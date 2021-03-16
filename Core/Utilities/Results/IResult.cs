8ac9f82a574345d9a9c15005539ec4ca3adc88a8
Mert Metin <mertmetin1993@gmail.com>
2021-03-17 02:10:12 +0300

    Core katmanına sonuç bilgilerini döndürecek Result,Success ve ErrorResult sınıfları eklendi.

---
+++ Core/Utilities/Results/IResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Utilities.Results
+{
+    //Temel void metotları için interfacedir.
+    public interface IResult
+    {
+        //Get: Sadece okunabilir.
+        bool Success { get; }
+        string Message { get; }
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,21 @@
+﻿namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        //Read onlyler constructorlarda set edilebilir.
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        //this kendini kastediyor. Result sınıfı
+        public Result(bool success,string message):this(success)
+        {
+            Message = message;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
